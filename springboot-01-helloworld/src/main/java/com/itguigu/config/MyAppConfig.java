7a4371a6abc14f72ef3a3977be00141d7d1f21e2
Haoming Chen <haomch@wicrenet.com>
2019-06-25 19:07:33 +0800

    @PropertySource @ImportResource @Bean Configuration File, yml Configuration File Block

---
+++ springboot-01-helloworld/src/main/java/com/itguigu/config/MyAppConfig.java
@@ -0,0 +1,8 @@
+package com.itguigu.config;
+
+/**
+ * @author Haoming Chen
+ * Created on 2019/6/25
+ */
+public class MyAppConfig {
+}
---
+++ springboot-01-helloworld/src/main/java/com/itguigu/service/HelloService.java
@@ -0,0 +1,8 @@
+package com.itguigu.service;
+
+/**
+ * @author Haoming Chen
+ * Created on 2019/6/25
+ */
+public class HelloService {
+}
