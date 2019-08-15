d9e7f93a58b8457de39aef2670b08493188b795c
Haoming Chen <haomch@wicrenet.com>
2019-08-15 18:25:29 +0800

    1. 添加用户完成

--- springboot-01-helloworld/src/main/java/com/itguigu/controller/EmployeeController.java
+++ springboot-01-helloworld/src/main/java/com/itguigu/controller/EmployeeController.java
@@ -4,10 +4,12 @@ import com.itguigu.dao.DepartmentDao;
 import com.itguigu.dao.EmployeeDao;
 import com.itguigu.entities.Department;
 import com.itguigu.entities.Employee;
+import org.hibernate.validator.constraints.CodePointLength;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -42,5 +44,18 @@ public class EmployeeController {
         Collection<Department> departments = departmentDao.getDepartments();
         model.addAttribute("depts",departments);
         return "emp/add";
+    }
+
+    //员工添加
+    //SpringMVC自动将请求参数和入参对象的属性进行一一绑定:
+    //要求了请求参数的名字和javaBean入参的对象里的属性名是一样的
+    @PostMapping("/emp")
+    public String addEmp(Employee employee){
+        //来到员工列表页面
+        employeeDao.save(employee);
+        System.out.println("保存的员工信息: " + employee);
+        //redirect:表示重定向到一个地址 /代表当前项目路径
+        //forward:表示转发到一个地址
+        return "redirect:/emps";
     }
 }
